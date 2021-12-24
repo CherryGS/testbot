93b59bedac4473b9c0a7a6369c440cba699b8759
CherryGS <uplliode@gmail.com>
2021-12-24 11:42:59 +0800

    1

---
+++ plugins/new/__init__.py
@@ -0,0 +1,8 @@
+from nonebot.adapters.cqhttp import Bot, Event
+from nonebot.typing import T_State
+from nonebot.plugin import export, on_command
+
+export = export()
+
+export.ignore_global_control = False
+
