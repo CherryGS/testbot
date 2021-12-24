465b71496bc5a84c6f739e38d722e9222f8b444d
CherryGS <uplliode@gmail.com>
2021-12-24 16:50:15 +0800

    upd

--- plugins/repeaterer/__init__.py
+++ plugins/repeaterer/__init__.py
@@ -6,15 +6,19 @@ from nonebot.plugin import on_message
 from nonebot.log import logger
 from nonebot.typing import T_State
 
-cmd = on_message()
+cmd = on_message(priority=10)
 dic = {}
 
 @cmd.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     msg = event.message
     id = event.group_id
-    print(msg)
+    flag = False
     if id in dic.keys():
         if dic[id] == msg:
-            await cmd.send(msg)
+            flag = True
     dic[id] = msg
+    if flag :
+        await cmd.finish(msg)
+    else:
+        return
